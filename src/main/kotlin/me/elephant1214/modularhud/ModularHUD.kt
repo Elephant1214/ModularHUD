9be615b7b2a5e7a92f2d187b75475c5207480d65
Elephant1214 <nathangetsmail@gmail.com>
2024-03-16 10:28:32 -0600

    Fix build

--- src/main/kotlin/me/elephant1214/modularhud/ModularHUD.kt
+++ src/main/kotlin/me/elephant1214/modularhud/ModularHUD.kt
@@ -1,11 +1,9 @@
 package me.elephant1214.modularhud
 
 import net.fabricmc.api.ModInitializer
-import org.slf4j.LoggerFactory
 
 object ModularHUD : ModInitializer {
-    private val LOGGER = LoggerFactory.getLogger("Modular HUD")
-
 	override fun onInitialize() {
+		// Stop detekt from complaining about this being empty
 	}
 }
